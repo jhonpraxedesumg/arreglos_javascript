e63e2ef167603d7f0121952d885556f9ea7e55d7
jhonpraxedesumg <140872091+jhonpraxedesumg@users.noreply.github.com>
2024-03-01 11:33:50 -0600

    Create crearApp.js

---
+++ archivos/crearApp.js
@@ -0,0 +1,9 @@
+//const salad = ['?', '?', '?', '?', '?', '?', '?'];
+//console.log(salad);
+
+
+const salad = new Array('praxedes', '?', '?', '?', '?', '?', 'Salazar');     //crear arreglo con un constructor
+
+//salad.push('praxedes');  //añadir elementos al final push, para añadir al principio es unshift  
+salad.shift();  //elemina elemeto al final , shift eliminar del principio
+console.log(salad);
